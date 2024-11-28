456950845c1f65b093f4823e678822bd1406e2da
Gabor Nemeth <gabor.d.nemeth@gmail.com>
2024-11-28 01:21:16 +0100

    Initialization

---
+++ src/main/java/org/gabor/nemeth/task/Main.java
@@ -0,0 +1,9 @@
+package org.gabor.nemeth.task;
+
+import org.gabor.nemeth.task.utils.TimeFormatter;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(TimeFormatter.formatDuration(12345));
+    }
+}
